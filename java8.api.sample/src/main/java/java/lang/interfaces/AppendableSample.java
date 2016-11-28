d4af674391dd0e967e9a0ad327e1c607d621d140
王银奎(银奎) <wangyk@uuzu.com>
2016-11-28 18:40:30 +0800

    add Appendable interface sample

---
+++ java8.api.sample/src/main/java/java/lang/interfaces/AppendableSample.java
@@ -0,0 +1,30 @@
+package java.lang.interfaces;
+
+import java.io.IOException;
+
+public class AppendableSample {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	class AppendableS1 implements Appendable {
+
+		public Appendable append(CharSequence csq) throws IOException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public Appendable append(CharSequence csq, int start, int end) throws IOException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public Appendable append(char c) throws IOException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
+}

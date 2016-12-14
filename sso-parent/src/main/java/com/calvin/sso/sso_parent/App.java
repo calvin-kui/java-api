363c093ead7970b7cc9c5f71082d9ea9340bfb0e
王银奎(银奎) <wangyk@uuzu.com>
2016-12-14 17:01:37 +0800

    sso demo

---
+++ sso-parent/src/main/java/com/calvin/sso/sso_parent/App.java
@@ -0,0 +1,13 @@
+package com.calvin.sso.sso_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
